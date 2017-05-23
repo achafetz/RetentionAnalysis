07856206a5716835020fdd0b87289416a5abb6fe
Aaron <achafetz@usaid.gov>
2017-05-23 17:55:31 -0400

    updates

--- DoFiles/retentionanalysis.r
+++ DoFiles/retentionanalysis.r
@@ -2,68 +2,154 @@
 # DRAFT
 # https://github.com/achafetz/RetentionAnalysis/wiki/Draft-R-Code
 
-library("tidyverse")
 
-setwd("C:/Users/achafetz/Documents/GitHub/RetentionAnalysis/Data")
-aisdata <- read.csv("BWA AIS relevant data.csv", header=TRUE, sep=",")
-crosswalk <- read.csv("District Crosswalk.csv", header = TRUE, sep = ",")
+## DEPENDENT LIBRARIES ##
+library("tidyverse")
+library(readxl)
+
+
+## DISTRICT CROSSWALK ACROSS DATASETS ##
+
+#import
+  setwd("C:/Users/achafetz/Documents/GitHub/RetentionAnalysis/Data")
+  crosswalk <- read.csv("District Crosswalk.csv", header = TRUE, sep = ",")
+
+
+## AIS DATA ##
+
+#import
+  aisdata <- read.csv("BWA AIS relevant data.csv", header=TRUE, sep=",")
 
 #rename for merge
-names(aisdata)[1] <- "ais_districts"
-
-
-aisdata <- dplyr::full_join(aisdata, crosswalk, by = "ais_districts")
-
-new <- dplyr::summarise_each(aisdata, funs(sum))
+  aisdata <- rename(aisdata, ais_districts = district)
 
 #TODO
-aisdata %>%
-    group_by(district) %>%
-    summarise_each(funs(toString))
+#convert percentages to raw numbers before aggregating up
+  for (x in c(media,drugs_male,drugs_female,drugs_bothsex,condom,prevention,attitude,tested_pct,access)) {
+    head <- colnames(select(aisdata,starts_with(i)))
+    for (y in head) {
+      if (x =="prevention" | x =="attitude") {
+        aisdata <- mutate(aisdata, y = (y/100)*total_heardofhiv)
+      }
+      else if (x =="tested_pct" | x =="access") {
+        aisdata <- mutate(aisdata, y = (y/100)*pop_total)
+      }
+      else {
+        aisdata <- mutate(aisdata, y = (y/100)*total_x)
+      }
+    }
+  }
+  
+  
+  
+  
+#merge & aggregate
+  aisdata <- aisdata %>%
+    left_join(crosswalk, by = "ais_districts") %>% #merge
+    subset(select = -c(fv_districts, ea_districts, ais_districts)) %>% #remove unnecessary district names
+    select(district, everything()) %>% #reorder
+    group_by(district) %>% 
+    summarise_each(funs(sum)) #aggregate by district
 
 
-# IMPORT FACT VIEW DATA
-# set the working directory(Wd), eg the file path where your data is stored --> change to folder path on your machine
-setwd("C:/Users/achafetz/Documents/ICPI/Data/")
-
+## FACT VIEW DATA ##
+  
 #import data and call the dataframe factviewdata
-fvdata <- read.table("ICPI_FactView_PSNU_IM_20170324_v2_2.txt", header = TRUE, sep = "\t", fill = TRUE)
+  setwd("C:/Users/achafetz/Documents/ICPI/Data/")
+  fvdata <- read.table("ICPI_FactView_PSNU_IM_20170324_v2_2.txt", header = TRUE, sep = "\t", fill = TRUE)
+
 # change all header names to lower case to make it easier to use
-names(fvdata) <- tolower(names(fvdata))
-# subset fv to Bots
-bots_fvdata <- fvdata %>%
-  filter(operatingunit == "Botswana", indicator == "TX_RET", disaggregate == "Total Denominator" | disaggregate == "Total Numerator") %>%
-  select(psnu, psnuuid, fy16snuprioritization, numeratordenom, fy2016apr)
+  names(fvdata) <- tolower(names(fvdata))
 
-#destring
-bots_fvdata$fy2016apr <-as.numeric(bots_fvdata$fy2016apr)
+# subset fv to Bots only & key indicators
+  bots_fvdata <- fvdata %>%
+    filter(operatingunit == "Botswana", indicator == "TX_RET", disaggregate == "Total Denominator" | disaggregate == "Total Numerator") %>%
+    select(psnu, psnuuid, fy16snuprioritization, numeratordenom, fy2016apr)
 
-#collapse
-bots_fvdata <- bots_fvdata %>%
-  group_by(psnu, psnuuid, fy16snuprioritization, numeratordenom) %>%
-  summarize_each(funs(sum), fy2016apr) %>%
-  ungroup
+#destring APR data
+  bots_fvdata$fy2016apr <-as.numeric(bots_fvdata$fy2016apr)
+
+#aggregate
+  bots_fvdata <- bots_fvdata %>%
+    group_by(psnu, psnuuid, fy16snuprioritization, numeratordenom) %>%
+    summarize_each(funs(sum), fy2016apr) %>%
+    ungroup
 
 #reshape wide
-bots_fvdata <- spread(bots_fvdata, numeratordenom, fy2016apr)
+  bots_fvdata <- spread(bots_fvdata, numeratordenom, fy2016apr)
 
-#gen
-bots_fvdata <-mutate(bots_fvdata, ret_pct = N/D)
-bots_fvdata$ret_pct <- round(bots_fvdata$ret_pct, 3)
+#gen retention percentages by district
+  bots_fvdata <-mutate(bots_fvdata, ret_pct = N/D)
+  bots_fvdata$ret_pct <- round(bots_fvdata$ret_pct, 3)
 
-rm(fvdata)
+#drop fv dataset
+  rm(fvdata)
 
-# IMPORT EA DATA NAV DATA
-library(readxl)
-#set working directory
-setwd("C:/Users/achafetz/ocuments/GitHub/RetentionAnalysis/Data")
+#rename districst for merge
+  bots_fvdata <- rename(bots_fvdata, fv_districts = psnu)
+
+#merge with district crosswalk
+  bots_fvdata <- bots_fvdata %>%
+    left_join(crosswalk, by = "fv_districts") %>% #merge
+    subset(select = -c(fv_districts, ea_districts, ais_districts, psnuuid)) %>% #remove unnecessary district names
+    select(district, everything()) #reorder
+
+  
+## IMPATT DATA ##
+
+#import Nat/SubNat data
+  impattdata <- read.table("ICPI_FactView_NAT_SUBNAT_20170324_v2_2.txt", header = TRUE, sep = "\t", fill = TRUE)
+
+# change all header names to lower case to make it easier to use
+  names(impattdata) <- tolower(names(impattdata))
+  
+  # subset fv to Bots only & key indicators
+  bots_impattdata <- impattdata %>%
+    filter(operatingunit == "Botswana", indicator == "PLHIV (SUBNAT)" | indicator == "TX_CURR_SUBNAT", disaggregate == "Total Numerator") %>%
+    select(psnu, psnuuid, fy16snuprioritization, indicator, fy2016)
+
+  rm(bots_impattdata)
+
+#rename indicators for reshape 
+  levels(bots_impattdata$indicator)[match("PLHIV (SUBNAT)",levels(bots_impattdata$indicator))] <- "plhiv"
+  levels(bots_impattdata$indicator)[match("TX_CURR_SUBNAT",levels(bots_impattdata$indicator))] <- "tx_curr_subnat"
+  
+#reshape wide
+  bots_impattdata <- spread(bots_impattdata, indicator, fy2016)
+
+#rename districst for merge
+  bots_impattdata <- rename(bots_impattdata, fv_districts = psnu)
+  
+#merge with district crosswalk
+  bots_impattdata <- bots_impattdata %>%
+    left_join(crosswalk, by = "fv_districts") %>% #merge
+    subset(select = -c(fv_districts, ea_districts, ais_districts, psnuuid)) %>% #remove unnecessary district names
+    select(district, everything()) #reorder    
+  
+## EA DATA NAV DATA ##
+
 #import EA data from Botswana data nav tool
-bots_eadata <- read_excel("Botswana 2016 EA Data Nav Tool v01.17.17.xlsm", sheet = "Totals_MCCNav")
-names(bots_eadata) <- tolower(names(bots_eadata))
+  setwd("C:/Users/achafetz/Documents/GitHub/RetentionAnalysis/Data")
+  bots_eadata <- read_excel("Botswana 2016 EA Data Nav Tool v01.17.17.xlsm", sheet = "Totals_MCCNav")
+
+# change all header names to lower case to make it easier to use
+  names(bots_eadata) <- tolower(names(bots_eadata))
+  
 #subset
 bots_eadata <- bots_eadata %>%
   filter(rptgcycle==2016 & data_type=="De-Dup") %>%
   select(national_sub_unit, datim_snu_id, cbcts_lnkg_exp, cbcts_rtnadhr_exp, fbcts_loaded_tot)
-#rename
-bots_eadata <- rename(bots_eadata, psnu = national_sub_unit)
-bots_eadata <- rename(bots_eadata, psnuuid = datim_snu_id)
+
+#rename for merge
+  bots_eadata <- rename(bots_eadata, ea_districts = national_sub_unit)
+  bots_eadata <- rename(bots_eadata, psnuuid = datim_snu_id)
+
+#merge with district crosswalk
+  bots_eadata <- bots_eadata %>%
+    left_join(crosswalk, by = "ea_districts") %>%
+    subset(select = -c(fv_districts, ea_districts, ais_districts)) %>%
+    select(district, everything())
+
+#merge all
+  bots <- full_join(bots_eadata, bots_fvdata, by="district")
+  bots <- full_join(bots, aisdata, by="district")
